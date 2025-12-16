dd29604904e3e8999bf15aeebdbfce94209c8736
Swaraj Shinde <sashinde74@gmail.com>
2025-12-16 11:13:08 +0530

    assi 2 q1 corrected

--- assignment 2/assi2_q1.c
+++ assignment 2/assi2_q1.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main(){
     unsigned int r = 0x2A;
-    printf("Original value : %8b",r);
+    printf("Original value : %8b\nhex value : %x",r,r);
     unsigned int a = 0x80;
     a=a>>3;
     r=r|a;
-    printf("\nSet bit 4 : %8b",r);
+    printf("\nSet bit 4 : %8b\nhex value : %x",r,r);
     a=0x80;
     a=a>>6;
     r=r&~(a);
-    printf("\nClear bit 1 : %8b",r);
+    printf("\nClear bit 1 : %8b\nhex value : %x",r,r);
     a=0x80;
     a=a>>2;
     r=r^a;
-    printf("\nToggle bit 5 : %8b",r);
+    printf("\nToggle bit 5 : %8b\nhex value : %x",r,r);
 }

02e2e6f12ff89512c25fc426c9474f7bbe6c215e
Swaraj Shinde <sashinde74@gmail.com>
2025-12-16 11:08:05 +0530

    q1 q2 corrected

--- assignment 2/assi2_q1.c
+++ assignment 2/assi2_q1.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
-    unsigned char r = 0x2A;
+    unsigned int r = 0x2A;
     printf("Original value : %8b",r);
-    unsigned char a = 0x80;
+    unsigned int a = 0x80;
     a=a>>3;
     r=r|a;
     printf("\nSet bit 4 : %8b",r);
--- assignment 2/assi2_q2.c
+++ assignment 2/assi2_q2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-    unsigned char s = 0x08;
-    unsigned char a = 0x80,b;
+    unsigned int s = 0x08;
+    unsigned int a = 0x80,b;
     a=a>>4;
     b=s&a;
     if(b==0x08){

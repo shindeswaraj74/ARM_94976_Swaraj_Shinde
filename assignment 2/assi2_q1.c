8405c2dec21db25ab7c85ac2b73f3a4223344519
Swaraj Shinde <sashinde74@gmail.com>
2025-12-16 09:38:10 +0530

    p1

---
+++ assignment 2/assi2_q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    unsigned int r = 0x2A;
+    printf("Original value : %8b",r);
+    unsigned int a = 0x80;
+    a=a>>3;
+    r=r|a;
+    printf("\nSet bit 4 : %8b",r);
+    a=0x80;
+    a=a>>6;
+    r=r&~(a);
+    printf("\nClear bit 1 : %8b",r);
+    a=0x80;
+    a=a>>2;
+    r=r^a;
+    printf("\nToggle bit 5 : %8b",r);
+}
---
+++ assignment 2/assi2_q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    unsigned int s = 0x08;
+    unsigned int a = 0x80,b;
+    a=a>>4;
+    b=s&a;
+    if(b==0x08){
+        printf("Bit 3 is set");
+    }
+    else{
+        printf("Bit 3 is clear");
+    }
+}

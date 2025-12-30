56d964c86fb84a6ff946f776398ef811c1537c4c
Swaraj Shinde <sashinde74@gmail.com>
2025-12-30 17:05:26 +0530

    sunbeam project

---
+++ 94976 project/Core/Inc/mq2.h
@@ -0,0 +1,16 @@
+/*
+ * mq2.h
+ *
+ *  Created on: Dec 30, 2025
+ *      Author: aupat
+ */
+
+#ifndef MQ2_H
+#define MQ2_H
+
+#include <stdint.h>
+
+uint32_t mq2_value(uint32_t adc_raw);
+
+#endif
+
